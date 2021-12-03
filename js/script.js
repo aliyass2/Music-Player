b986d5627b460840596a6fa04bb123f96078afca
aliyass2 <93873815+aliyass2@users.noreply.github.com>
2021-12-03 17:27:45 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+var loop = document.getElementById("loop");
+icon.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    icon.src = "/images/pause.png";
+  } else {
+    mySong.pause();
+    icon.src = "/images/play.png";
+  }
+};
+// تحديد دالة myBtn بعد ما يقوم الشخص بالضغط على الزر سوف تعمل الدالة
+document.getElementById("myBtn").onclick = function () {
+  myFunction();
+};
+
+/* toggle between hide and show */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+loop.onclick = function () {
+  mySong.loop = true;
+};
